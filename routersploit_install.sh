7a70d0cd5e610c4f4fca286ce2438889ca7153b3
SrgokuBlack <45349176+SrgokuBlack@users.noreply.github.com>
2019-02-12 14:50:59 -0300

    Add files via upload

---
+++ routersploit_install.sh
@@ -0,0 +1,9 @@
+apt update
+apt install clang
+apt install make
+apt install python
+apt install python-dev
+apt install openssl-dev
+apt install libffi-dev
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
